f9919e00ccd175beb59754c451387f68a11e6e36
deh410 <aearahawa@gmail.com>
2020-07-27 00:19:53 -0300

    import db config and migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from .models import configure as config_db
 
 def create_app():
   app = Flask(__name__)
@@ -8,5 +10,9 @@ def create_app():
 
   # remove error from track mod
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+  config_db(app)
+
+  Migrate(app, app.db)
 
   return app
