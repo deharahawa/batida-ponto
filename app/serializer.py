492c61645e607de47376c7329a768849387f497f
deh410 <aearahawa@gmail.com>
2020-07-27 00:10:10 -0300

    define the Ponto schema

--- app/serializer.py
+++ app/serializer.py
@@ -13,8 +13,20 @@ class UserSchema(Schema):
     self_view_kwargs = {'id': '<id>'}
     self_view_many = 'user_many'
 
-    id = fields.Integer(dump_only=True)
-    nome_completo = fields.Str(required=True, validate=must_not_be_blank)
-    cpf = fields.Str(required=True,validate=must_not_be_blank)
-    email = fields.Str(required=True, validate=must_not_be_blank)
-    data_cadastro = fields.DateTime(dump_only=True)
+  id = fields.Integer()
+  nome_completo = fields.Str(required=True, validate=must_not_be_blank)
+  cpf = fields.Str(required=True,validate=must_not_be_blank)
+  email = fields.Str(required=True, validate=must_not_be_blank)
+  data_cadastro = fields.DateTime(dump_only=True)
+
+
+class PontoSchema(Schema):
+  # class Meta:
+  #   type_ = 'ponto'
+  #   self_view = 'ponto_one'
+  #   self_view_kwargs = {'id': '<id>'}
+  id = fields.Integer()
+  user = fields.Nested(UserSchema, validate=must_not_be_blank)
+  data_batida = fields.DateTime(dump_only=True)
+  tipo_batida = fields.Integer()
+
