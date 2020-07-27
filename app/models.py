175f3f67ad2c25f2098eee033385053500bee433
deh410 <aearahawa@gmail.com>
2020-07-26 23:43:49 -0300

    define User model with attributes

--- app/models.py
+++ app/models.py
@@ -5,3 +5,11 @@ db = SQLAlchemy()
 def configure(app):
   db.init_app(app)
   app.db = db
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  nome_completo = db.Column(db.String(255))
+  cpf = db.Column(db.Integer(11))
+  email = db.Column(db.String(255))
+  data_cadastro = db.Column(db.Date())
+
