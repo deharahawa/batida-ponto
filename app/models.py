ba93e48e30fdc508f4a490c673a521dade9806c2
deh410 <aearahawa@gmail.com>
2020-07-26 23:50:13 -0300

    define Ponto model and change Date for DateTime

--- app/models.py
+++ app/models.py
@@ -11,5 +11,11 @@ class User(db.Model):
   nome_completo = db.Column(db.String(255))
   cpf = db.Column(db.Integer(11))
   email = db.Column(db.String(255))
-  data_cadastro = db.Column(db.Date())
+  data_cadastro = db.Column(db.DateTime)
 
+class Ponto(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  user = db.relationship('User')
+  data_batida = db.column(db.DateTime)
+  tipo_batida = db.column(db.Integer(1))
