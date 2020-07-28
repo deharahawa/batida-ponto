8074220938ae0fb4c47bd1370d16db89c0e961c4
deh410 <aearahawa@gmail.com>
2020-07-28 17:10:30 -0300

    comment models and relationships

--- app/models.py
+++ app/models.py
@@ -5,35 +5,35 @@ from .serializer import UserSchema
 db = SQLAlchemy()
 
 def configure(app):
+  """
+  Factory para poder configurar
+  """
+  # Inicializa o app
   db.init_app(app)
   with app.app_context():
+    # Cria as alteracoes usando o contexto
     db.create_all()
   app.db = db
 
 class User(db.Model):
+  """
+  Define a class que reprenta o model do User
+  """
   id = db.Column(db.Integer, primary_key=True)
   nome_completo = db.Column(db.String(255))
   cpf = db.Column(db.String(11))
   email = db.Column(db.String(255))
   data_cadastro = db.Column(db.DateTime)
-  # pontos = db.relationship('Ponto', backref='usuario', lazy=True)
 
 
 class Ponto(db.Model):
+  """
+  Define a class que reprenta o model do Ponto
+  """
   id = db.Column(db.Integer, primary_key=True)
+  # Define a chave estrangeira do relacionamento 1 para muitos
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  # Define o relacionamento entre user e pontos
   user = db.relationship('User', backref='checks')
   data_batida = db.Column(db.DateTime)
   tipo_batida = db.Column(db.Integer)
-
-
-class UserOne(ResourceList):
-  schema = UserSchema
-  data_layer = {'session': db.session,
-                'model': User}
-
-
-class UserMany(ResourceDetail):
-  schema = UserSchema
-  data_layer = {'session': db.session,
-                'model': User}
